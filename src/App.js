34fb93a6592233ce361bb0fd40eaf0011b639a8f
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2023-04-18 14:32:21 -0700

    React-router-dom added to app.js to correspond URL path to component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -9,14 +11,17 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <About />
-      <Work />
-      <Contact />
+   
+    <Router>
+       <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/work" component={Work} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
