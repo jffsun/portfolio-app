4c02d4be0554c9ec1bf9422a6f915c616c37d993
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2023-04-18 18:56:07 -0700

    Remote router navigation to instead use single page and react-scroll

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Work = () => {
   return (
-    <div className="work">
+    <div className="work" id="work">
       <h2>My Work</h2>
       <p>[List your previous work experiences or projects, with brief descriptions and links to their respective websites (if applicable)].</p>
     </div>
