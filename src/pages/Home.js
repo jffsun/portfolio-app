2f0ab65aa1821c81d6073b3f32bfd1d82ed4a559
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2023-04-29 14:47:15 -0700

    Home page styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Home = ({ pageTitle }) => {
   
   return (
-    <div className="home" id="home">
-      <h3>{pageTitle}</h3>
-      <h1>My name is Jeffrey, and I am a Full Stack Developer based in Irvine.</h1>
+    <div className="home-container" id="home">
+      <div className="home-contents">
+        <h1>My name is Jeffrey, and I am a Full Stack Developer based in Irvine.</h1>
+      </div>
     </div>
   );
 };
