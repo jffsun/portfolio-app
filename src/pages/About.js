1cb56b7b8e68e3bd728acac26b30f03d81ff5898
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2023-05-05 16:01:32 -0700

    Navbar dropdown styling for links; Add to home page text; Spacing between about blurbs

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,36 +4,28 @@ const About = () => {
   return (
     <div className="about-container" id="about">
       <div className="about-header">
-        <h2>About Me</h2>
+        <h2>About</h2>
       </div>
       <div className="about-content">
-      <h3>Who am I?</h3>
-        <p>
-          {/* Certificate / graduation logo */}
-          Full Stack Developer and recent graduate of University of California, Irvine's Coding Bootcamp.
-        </p>
-        <p>
-          Diverse professional background of Finance and Human Resources provides me a unique combination of technical and business skills.
-        </p>
+      <div className="about-blurb">
+        <h3>About me</h3>
+        <p>{/* Certificate / graduation logo */} Full Stack Developer and recent graduate of University of California, Irvine's Coding Bootcamp.</p>
+        <p> Diverse professional background of Finance and Human Resources provides me a unique combination of technical and business skills.</p>
+      </div>
+      <div className="about-blurb">
         <h3>Front-end & Back-end Technologies</h3>
-        <p>
-          Python, JavaScript, HTML, CSS, React, Redux, jQuery, SQL, MongoDB, Node.js, Express.js
-          {/* Insert logos for each side by side */}
-        </p>
-        <h3>
-          What am I doing right now?
-        </h3>
-        <p>
-          Pursuing AWS Cloud Practitioner certification
-        </p>
-        <p>
-          Actively seeking new career opportunities (Open to relocate for the right opportunity)
-        </p>
+        <p>Python, JavaScript, HTML, CSS, React, Redux, jQuery, SQL, MongoDB, Node.js, Express.js{/* Logos for each side by side with hover squiggle */}</p>
+      </div>
+      <div className="about-blurb">
+        <h3>What am I doing right now?</h3>
+        <p>Pursuing AWS Cloud Practitioner certification{/* AWS Logo */}</p>
+        <p>Actively seeking new career opportunities (Open to relocate for the right opportunity)</p>
+      </div>
+      <div className="about-blurb">
         <h3>What do I do when I'm not coding?</h3>
-        <p>
-          Lift weights in the gym, take my dog Raven on walks, listen to neuroscience podcasts, and run on trails as I train for my first marathon!
-          {/* Animation of brain training / working out */}
+        <p> Lift weights in the gym, take my dog Raven on walks, listen to neuroscience podcasts, and run on trails as I train for my first marathon!{/* Animation of brain running */}
         </p>
+      </div>
       </div>
     </div>
   );
