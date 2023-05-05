56e5e2340bd8416ffc759f19db9bbffaaa703e8c
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2023-05-05 16:01:32 -0700

    Navbar dropdown styling for links; Add to home page text; Spacing between about blurbs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,10 @@ const Home = ({ pageTitle }) => {
   return (
     <div className="home-container" id="home">
       <div className="home-content">
-        <h1>My name is Jeffrey, and I am a</h1>
-        <h1>Full Stack Developer based in</h1>
-        <h1>Southern California.</h1>
+        <h1>Hello.</h1>
+        <h1>My name is Jeffrey,</h1>
+        <h1>and I'm a Full Stack Developer</h1>
+        <h1>based in Irvine, California.</h1>
       </div>
     </div>
   );
